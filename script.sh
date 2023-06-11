6c557e53e5a61ed22339f72b93fb53dd3b09aa8f
ganeshkinkar <51238163+ganeshkinkar@users.noreply.github.com>
2023-06-11 21:40:08 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-GHE_TOKEN=$1
+TOKEN=$1
+
+echo $TOKEN
 
 # Create new repo
 
 curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $GHE_TOKEN"\
+  -H "Authorization: Bearer $TOKEN"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/orgs/ganesh-document/repos \
   -d '{"name":"Hello-World","description":"This is your first repository","homepage":"https://github.com","private":false,"has_issues":true,"has_projects":true,"auto_init":true}'
